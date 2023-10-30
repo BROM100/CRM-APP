4ec3c26b02620066302b1c7203403a0e356228a5
BROM100 <bartekantos@protonmail.com>
2023-10-30 23:30:05 +0100

    Added new icons for click action, function for changing pages.

--- main.py
+++ main.py
@@ -19,6 +19,51 @@ class MainWindow(QMainWindow):
         self.ui.stackedWidget.setCurrentIndex(0)
         self.ui.home_btn2.setChecked(True)
 
+    def on_home_btn1_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(0)
+
+    def on_home_btn2_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(0)
+
+    def on_dashboard_btn1_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(2)
+
+    def on_dashboard_btn2_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(2)
+
+    def on_orders_btn1_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(3)
+
+    def on_orders_btn2_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(3)
+
+    def on_products_btn1_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(4)
+
+    def on_products_btn2_toggled(self, ):
+        self.ui.stackedWidget.setCurrentIndex(4)
+
+    def on_customers_btn1_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(5)
+
+    def on_customers_btn2_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(5)
+
+    def on_contacts_btn1_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(6)
+
+    def on_contacts_btn2_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(6)
+
+    def on_leads_btn1_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(7)
+
+    def on_leads_btn2_toggled(self):
+        self.ui.stackedWidget.setCurrentIndex(7)
+
+    def on_user_btn_clicked(self):
+        self.ui.stackedWidget.setCurrentIndex(1)
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
